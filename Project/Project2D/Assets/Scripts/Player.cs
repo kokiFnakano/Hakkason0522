b1d0981a21c86ca19a1fe5e87c182f5be5999bca
negitama.koki@outlook.jp <negitamaikok@bitbucket.org>
2021-05-22 16:45:22 +0900

    [fix]ボタン選択のエラー直し

--- Project/Project2D/Assets/Scripts/Player.cs
+++ Project/Project2D/Assets/Scripts/Player.cs
@@ -7,15 +7,20 @@ using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] MoneyButton defaultButton;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        EventSystem.current.SetSelectedGameObject(defaultButton.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(EventSystem.current.currentSelectedGameObject == null)
+            EventSystem.current.SetSelectedGameObject(defaultButton.gameObject);
     }
 
 
@@ -23,6 +28,7 @@ public class Player : MonoBehaviour
     //ベットした値を取得
     public int GetRaiseValue()
     {
+        Debug.Log(EventSystem.current.currentSelectedGameObject.name);
         return EventSystem.current.currentSelectedGameObject.GetComponent<MoneyButton>().GetValue();
     }
 
@@ -31,6 +37,6 @@ public class Player : MonoBehaviour
     //ボタンの選択フラグをオフにする
     public void ResetButtons()
     {
-        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(defaultButton.gameObject);
     }
 }
