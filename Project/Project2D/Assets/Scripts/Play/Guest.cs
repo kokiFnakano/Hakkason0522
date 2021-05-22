94f041aa1a1274902fea05e3cb51977b95e7ec3b
nekota <riarunekota@gmail,com>
2021-05-22 14:35:24 +0900

    [add] 客の入札の中身追加

--- Project/Project2D/Assets/Scripts/Play/Guest.cs
+++ Project/Project2D/Assets/Scripts/Play/Guest.cs
@@ -7,10 +7,23 @@ public class Guest : MonoBehaviour
     [SerializeField]
     private int m_targetMoney;
 
+    // Š‹à
+    [SerializeField]
+    private int m_haveMoney;
+
+    // —~‚µ‚¢“x
+    private float m_mood = 1.0f;
+
+
+    // ƒLƒƒƒbƒVƒ…‚µ‚Ä‚¨‚­
+    Voltage voltage;
+    Goods goods;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        voltage = FindObjectOfType<Voltage>();
+        goods = FindObjectOfType<Goods>();
     }
 
     // Update is called once per frame
@@ -19,9 +32,33 @@ public class Guest : MonoBehaviour
         
     }
 
+    public void UpdateMood()
+    {
+        
+    }
+
     public int Bidding()
     {
         int bidNum = 0;
+
+        // “üDˆÓ—~‚ğZo(‰¼)
+        float bidMotivation = m_mood * voltage.GetVoltageValue();
+
+        // “üD‚·‚é‚©‚Ç‚¤‚©
+        if (Random.Range(0.0f, 1.0f) < bidMotivation)
+        {
+            // “üD
+
+            // “üD‹àŠz‚ğŒvZ
+            bidNum = Random.Range(1, 10) * 10000;
+
+        }
+        else
+        {
+            bidNum = 0;
+        }
+        
+
         return bidNum;
     }
 }
